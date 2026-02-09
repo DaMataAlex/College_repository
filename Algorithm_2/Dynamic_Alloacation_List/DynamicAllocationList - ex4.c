74d046c112db9d2abd37acf58901e4eb82ebedc0
Alex Ribeiro <alex.rbribeiro@gmail.com>
2026-02-09 16:57:19 -0300

    Update DynamicAllocationList - ex4.c

--- Algorithm_2/Dynamic_Alloacation_List/DynamicAllocationList - ex4.c
+++ Algorithm_2/Dynamic_Alloacation_List/DynamicAllocationList - ex4.c
@@ -1,52 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 
 void limpar_terminal(){
-    #ifdef _WIN32
-        system("cls");
-    #else
-        system("clear");
-    #endif
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
 }
 
-typedef struct{
-    double reais;
-}vetor;
-
 int menu(){
-    printf("Defina o tamanho do vetor: ");
     int tamanho_vetor;
+    printf("Defina o tamanho do vetor: ");
     scanf("%d", &tamanho_vetor);
-    getchar();
     return tamanho_vetor;
 }
 
-void fazer_vetor(vetor *novo_vetor, int tamanho_vetor){
-    for(int i = 0; i < tamanho_vetor; i++){
-        scanf("%lf", &novo_vetor[i].reais);
+void fazer_vetor(int *novo_vetor, int *tamanho_vetor){
+    // leitura do vetor
+    for(int i = 0; i < *tamanho_vetor; i++){
+        scanf("%d", &novo_vetor[i]);
     }
-    limpar_terminal();
-    printf("Vetor inserido com sucesso");
-    getchar();
-    getchar();
-    limpar_terminal();
-    printf("Seu vetor eh:\n");
-    for(int i = 0; i < tamanho_vetor; i++){
-        printf("%.0lf ", novo_vetor[i].reais);
+
+    for(int i = 0; i < *tamanho_vetor; i++){
+        for(int j = i + 1; j < *tamanho_vetor; ){
+            if(novo_vetor[i] == novo_vetor[j]){
+                
+                for(int k = j; k < *tamanho_vetor - 1; k++){
+                    novo_vetor[k] = novo_vetor[k + 1];
+                }
+                (*tamanho_vetor)--;
+            } else {
+                j++;
+            }
+        }
     }
-    getchar();
+
     limpar_terminal();
+    printf("Seu vetor sem duplicatas:\n");
+    for(int i = 0; i < *tamanho_vetor; i++){
+        printf("%d ", novo_vetor[i]);
+    }
+    printf("\n");
 }
 
 int main(){
-
-    vetor *vetor_usuario = NULL;
+    int *vetor_usuario = NULL;
     int tamanho_vetor = menu();
 
-    vetor_usuario = realloc(vetor_usuario, tamanho_vetor * sizeof(vetor));
-    fazer_vetor(vetor_usuario, tamanho_vetor);
+    vetor_usuario = malloc(tamanho_vetor * sizeof(int));
+    if(vetor_usuario == NULL){
+        printf("Erro de alocacao de memoria\n");
+        return 1;
+    }
 
+    fazer_vetor(vetor_usuario, &tamanho_vetor);
+
+    free(vetor_usuario);
     return 0;
 }
